186db7872c8edc409922579a4d2d1d51b79f0a45
Roy <b.roynaim@gmail.com>
2021-03-04 18:42:47 +0100

    Esercizio completato

--- main.js
+++ main.js
@@ -3,10 +3,25 @@
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
-var surname = .toUpperCase(prompt("What's your last name?"));
+var surname = prompt("What's your last name?");
 
-var surnamelist = ["Bianchi ", "Rossi ", "Perlari ", "Lodigiani ", "Mastrobattista ", "Verdi "];
+surname = surname.charAt(0).toUpperCase() + surname.slice(1); //charAt prendi l'elemento (carattere soprattutto) in pos. 0, applicchi poi metod. uppercase alla 1st lettera.
+//slice method: ti ritorna indietro l'inizio...se vado a 1...
+// surname = surname[0].toUpperCase() + surname.substring(1);
+
+var surnamelist = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
+
 
 surnamelist.push(surname);
 
-document.getElementById('list').innerHTML = surnamelist.sort();
+console.log(surnamelist.sort());
+
+for(var i=0; i<surnamelist.length; i++){
+  if(surnamelist[i] == surname){
+     flag = i;
+     flag++;
+  }  document.getElementById('list').innerHTML += "<li>" + surnamelist[i] + "</li>";
+}
+
+document.getElementById('result').innerHTML = (surnamelist.indexOf(surname) + 1);
+// i++ end
