5d669b91285e848155582c0b1c1ed20bd3003923
Roy <b.roynaim@gmail.com>
2021-03-04 17:31:46 +0100

    uppercase

--- main.js
+++ main.js
@@ -3,10 +3,10 @@
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
-var surname = prompt("What's your last name?");
+var surname = .toUpperCase(prompt("What's your last name?"));
 
-var surnamelist = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
+var surnamelist = ["Bianchi ", "Rossi ", "Perlari ", "Lodigiani ", "Mastrobattista ", "Verdi "];
 
 surnamelist.push(surname);
 
-console.log(surnamelist.sort());
+document.getElementById('list').innerHTML = surnamelist.sort();
